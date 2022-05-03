c3b0df3176b5bb1f3ce5cc66d7c6617e7d9a8764
Kundan Prajapati <prajapatikundan0@gmail.com>
2022-05-03 16:54:00 +0530

    Learn basic operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, take, map, filter, of, mergeMap, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs-project-zero';
+
+  ngOnInit() {
+    const numbers$ = interval(1000);
+    // numbers$.subscribe((payLoad: any) => console.log(payLoad));
+
+    // numbers$
+    //   .pipe(
+    //     take(5),
+    //     filter((x: any) => x % 2 !== 0),
+    //     map((x: any) => x * 100)
+    //   )
+    //   .subscribe((x: any) => console.log('first subscriber:', x));
+
+    // mergeMap operator
+    // const letters$ = of('a', 'b', 'c', 'd', 'e');
+    // letters$
+    //   .pipe(
+    //     mergeMap((x: any) => {
+    //       return numbers$.pipe(
+    //         take(5),
+    //         map((i: any) => i + x)
+    //       );
+    //     })
+    //   )
+    //   .subscribe((x: any) => console.log('second subscriber:', x));
+
+    const letters$ = of('a', 'b', 'c', 'd', 'e');
+    letters$
+      .pipe(
+        switchMap((x: any) => {
+          return numbers$.pipe(
+            take(5),
+            map((i: any) => i + x)
+          );
+        })
+      )
+      .subscribe((x: any) => console.log('second subscriber:', x));
+  }
 }
