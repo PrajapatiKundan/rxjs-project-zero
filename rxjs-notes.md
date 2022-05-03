7151d3361e152520d1de70b37f2e436ab72215a7
Kundan Prajapati <prajapatikundan0@gmail.com>
2022-05-03 18:20:34 +0530

    Implement search feature

--- rxjs-notes.md
+++ rxjs-notes.md
@@ -5,12 +5,12 @@
 - Best practice to add $ sign at the end of observable object
 - Observer is an Object with three properties as a method: next, error, complete
 
-# Observable
+## Observable
 
 - Observable is a function that take observer as argument and provide lots of methods like create(), subscribe()
 - Observable is an outer container
 
-# Observer
+## Observer
 
 - Observer is an object that has three methods: next, error, completed
 - Observer is an inner trigger
@@ -20,6 +20,10 @@
 > - Each observable contains its own observable and they are not shareable and reusable.
 > - Observers cannot interact with each other
 
-# Subject
+## Subject
 
 - Subject is both observable and observer
+
+## Resources
+
+[Operator Decision Tree](https://rxjs.dev/operator-decision-tree)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import {
+  Subject,
+  debounceTime,
+  map,
+  Observable,
+  switchMap,
+  distinctUntilChanged,
+} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +16,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs-project-zero';
+  searchString: string = '';
+  searchSubject$ = new Subject<string>();
+  results$ = new Observable<any>();
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    // const clicks = fromEvent(document, 'click');
+    // clicks.subscribe((x: any) => console.log(x));
+
+    this.results$ = this.searchSubject$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(), // skip value which is identical to previous value
+      switchMap((searchString) => this.queryAPI(searchString))
+    );
+  }
+
+  inputChanged($event: any) {
+    console.log('input changed', $event);
+    this.searchSubject$.next($event);
+  }
+
+  queryAPI(searchString: string) {
+    console.log('queryAPI', searchString);
+    return this.http
+      .get(`https://www.reddit.com/r/aww/search.json?q=${searchString}`)
+      .pipe(map((result: any) => result['data']['children']));
+  }
 }
