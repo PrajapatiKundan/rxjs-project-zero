22ca5e547cb14b17c19ead518950fd031808ed80
Kundan Prajapati <prajapatikundan0@gmail.com>
2022-05-03 13:35:47 +0530

    Learn basics of observable and observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { observable, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs-project-zero';
+  observable$: any;
+
+  ngOnInit() {
+    this.observable$ = new Observable((observer) => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.complete();
+    });
+
+    this.observable$.subscribe({
+      next: (value: any) => console.log(value),
+      error: () => console.log('Error'),
+      complete: () => console.log('This is the end'),
+    });
+  }
+
+  ngOnDestroy() {
+    this.observable$.unsubscribe();
+  }
 }
